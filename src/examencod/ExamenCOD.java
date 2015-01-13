d7c24d23e91d8104673f9cf83033b55d0b6aa98e
micaelr <micaelr@169.254.134.34>
2015-01-13 13:18:00 +0100

    commit 3 cambiar nombres

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -8,30 +8,30 @@ public class ExamenCOD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int iN = 11;
-        if (metodoMio(iN)) {
+        int numeroInicial = 11;
+        if (compararpares(numeroInicial)) {
             System.out.println("Es numero primo");
         } else {
             System.out.println("NO es numero primo");
         }
 
-        for (int x = 2; x < 1000; x++) {
-            if (metodoMio(x)) {
-                System.out.print(x + " ");
+        for (int iniciarContador = 2; iniciarContador < 1000; iniciarContador++) {
+            if (compararpares(iniciarContador)) {
+                System.out.print(iniciarContador + " ");
             }
         }
     }
 
-    public static boolean metodoMio(int variable) {
-        int j = 2;
-        boolean aux = true;
-        while ((aux) && (j != variable)) {
-            if (variable % j == 0) {
-                aux = false;
+    public static boolean compararpares(int numero) {
+        int ini = 2;
+        boolean auxiliar = true;
+        while ((auxiliar) && (ini != numero)) {
+            if (numero % ini == 0) {
+                auxiliar = false;
             }
-            j++;
+            ini++;
         }
-        return aux;
+        return auxiliar;
     }
 
 }
